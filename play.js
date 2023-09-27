494e54d893b25b2ddafc97be448d862a404d11c6
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-27 03:25:29 +0000

    move experiment

--- play.js
+++ play.js
@@ -8,6 +8,8 @@ const conn = connect('localhost', 50541);
 conn.on('connect', () =>{ 
   console.log('connected to server');
   conn.write('Name: MXJ');
+  // conn.write('Move: up');
+  // setInterval(() => conn.write('Move: up'), 100);
 });
 
 conn.on('data', print);
