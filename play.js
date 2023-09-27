5a242d56d40eee339c1db6eeaa00b65049e2f530
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-27 03:18:37 +0000

    write name field

--- play.js
+++ play.js
@@ -5,5 +5,9 @@ const { connect } = require('./client.js')
 console.log("Connecting ...");
 const conn = connect('localhost', 50541);
 
-conn.on('connect', () => console.log('connected to server', conn));
+conn.on('connect', () =>{ 
+  console.log('connected to server');
+  conn.write('Name: MXJ');
+});
+
 conn.on('data', print);
