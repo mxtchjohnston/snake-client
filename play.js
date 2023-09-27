fb36454914f1292fa6a1c4d503cbb80dc7207f78
mxtchjohnston <mitchjohnston99@gmail.com>
2023-09-27 03:34:29 +0000

    stdin

--- play.js
+++ play.js
@@ -1,9 +1,27 @@
 const print = (data) => console.log(data);
 const { connect } = require('./client.js')
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
 console.log("Connecting ...");
 const conn = connect('localhost', 50541);
+
+const handleUserInput = key => {
+  if (key === '\u0003') {
+    console.log("Disconnecting...")
+    process.exit();
+  }
+  
+};
+
+const stdin = setupInput();
+stdin.on("data", handleUserInput);
 
 conn.on('connect', () =>{ 
   console.log('connected to server');
